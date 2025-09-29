6da83699aa3fb2c911374f89d1ddfd5022c0e740
Robbie Plankenhorn <rplankenhorn@gmail.com>
2025-09-29 09:33:33 -0400

    Ensuring mqttClient is connected before publishing home assistant topics.

--- kegboard.ino
+++ kegboard.ino
@@ -294,17 +294,17 @@ void setup() {
 
   mqttClient.connect(MQTT_TOPIC, MQTT_USERNAME, MQTT_PASSWORD);
 
-  for (int i = 0; i < NUM_METERS; i++) {
-    String meterTopic = String::format("homeassistant/sensor/kegbot_meter_%i/config", i);
-    String meterPayload = String::format("{\"name\":\"Flow Meter %i\",\"state_topic\":\"kegbot/meter/%i\",\"unit_of_measurement\":\"pulses\",\"unique_id\":\"kegbot_flow_%i\",\"device\":{\"identifiers\":[\"kegbot_main\"],\"name\":\"Kegbot\"}}", i, i, i);
-    mqttClient.publish(meterTopic.c_str(), meterPayload.c_str(), true);
+  if (mqttClient.isConnected()) {
+    for (int i = 0; i < NUM_METERS; i++) {
+      String meterTopic = String::format("homeassistant/sensor/kegbot_meter_%i/config", i);
+      String meterPayload = String::format("{\"name\":\"Flow Meter %i\",\"state_topic\":\"kegbot/meter/%i\",\"unit_of_measurement\":\"pulses\",\"unique_id\":\"kegbot_flow_%i\",\"device\":{\"identifiers\":[\"kegbot_main\"],\"name\":\"Kegbot\"}}", i, i, i);
+      mqttClient.publish(meterTopic.c_str(), meterPayload.c_str(), true);
 
-    String tempTopic = String::format("homeassistant/sensor/kegbot_temp_%i/config", i);
-    String tempPayload = String::format("{\"name\":\"Temperature %i\",\"state_topic\":\"kegbot/temp/%i\",\"unit_of_measurement\":\"C\",\"device_class\":\"temperature\",\"unique_id\":\"kegbot_temp_%i\",\"device\":{\"identifiers\":[\"kegbot_main\"],\"name\":\"Kegbot\"}}", i, i, i);
-    mqttClient.publish(tempTopic.c_str(), tempPayload.c_str(), true);
+      String tempTopic = String::format("homeassistant/sensor/kegbot_temp_%i/config", i);
+      String tempPayload = String::format("{\"name\":\"Temperature %i\",\"state_topic\":\"kegbot/temp/%i\",\"unit_of_measurement\":\"°C\",\"device_class\":\"temperature\",\"unique_id\":\"kegbot_temp_%i\",\"device\":{\"identifiers\":[\"kegbot_main\"],\"name\":\"Kegbot\"}}", i, i, i);
+      mqttClient.publish(tempTopic.c_str(), tempPayload.c_str(), true);
+    }
   }
-
-  mqttPending = 1;
 
   SETUP_METER(0);
   SETUP_METER(1);
